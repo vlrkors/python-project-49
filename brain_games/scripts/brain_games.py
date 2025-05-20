4e71471bc811bb21341ad2f75b620e77e744f707
vlrkors <vlrkors@gmail.com>
2025-05-21 00:26:49 +0300

    feat: add fixbug

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 import prompt
 
+
 def main():
     print("Welcome to brain-games!")
     name = prompt.string('May I have your name? ')
     print("Hello, ", name)
     
+
 if __name__ == "__main__":
     main()
