6c0c5df1cb17eb2d2090656af1c6b26a12b0fcc1
vlrkors <vlrkors@gmail.com>
2025-05-20 23:31:10 +0300

    feat: add input string

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,8 @@
+import prompt
 def main():
     print("Welcome to brain-games!")
-
-
+    name = prompt.string('May I have your name? ')
+    print("Hello, ", name)
+    
 if __name__ == "__main__":
     main()
