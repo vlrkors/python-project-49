51e40a2cc9146259f701b3558208a704e658d21a
vlrkors <vlrkors@gmail.com>
2025-05-21 08:11:04 +0300

    feat: correct architecture

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ brain-games:
 
 package-install:
 	uv tool install dist/*.whl
-
+	
 lint:
-    uv run ruff check brain_games
+	uv run ruff check brain_games
 	
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,6 @@
 import prompt
 
-from brain_games.scripts.services import get_random_number
+from brain_games.services.services import get_random_number
 
 
 def main():
@@ -8,9 +8,10 @@ def main():
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
     print('Answer "yes" if the number is even, otherwise answer "no"')
-    number_question = get_random_number
+    number_question = get_random_number()
     print(f'Question: {number_question}')
-
+    answer = prompt.string('Your answer: ')
+    
 
 if __name__ == "__main__":
     main()
