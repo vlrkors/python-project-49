8a0fd79f0d503818dc59d75c472b16bf19271faa
raev_va <vlrkors@gmail.com>
2025-05-21 07:48:09 +0300

    feat: add get number_question

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
 import prompt
 
+from brain_games.scripts.services import get_random_number
+
 
 def main():
     print("Welcome to brain-games!")
     name = prompt.string('May I have your name? ')
-    print("Hello, ", name)
-    
-
+    print(f"Hello, {name}!")
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+    number_question = get_random_number
+    print(f'Question: {number_question}')
 if __name__ == "__main__":
     main()
