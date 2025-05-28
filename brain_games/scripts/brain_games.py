c599b6aec85b9198b237b5a646b31ab17eed7b8c
vlrkors <vlrkors@gmail.com>
2025-05-28 23:06:47 +0300

    use welcome_user from cli in brain_games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,11 +2,10 @@ import prompt
 
 from brain_games.services.services import get_random_number
 
+from ..brain_games import cli
 
 def main():
-    print("Welcome to brain-games!")
-    name = prompt.string('May I have your name? ')
-    print(f"Hello, {name}!")
+    cli.welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no"')
     number_question = get_random_number()
     print(f'Question: {number_question}')
