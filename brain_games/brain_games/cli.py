fbdf24fc18926d8314220d8aae575573fb591ff4
vlrkors <vlrkors@gmail.com>
2025-05-28 23:13:49 +0300

    add fis linter recommend

--- brain_games/brain_games/cli.py
+++ brain_games/brain_games/cli.py
@@ -1,6 +1,8 @@
 import prompt
 
+
 def welcome_user():
+
     print("Welcome to brain-games!")
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
--- brain_games/scripts/main.py
+++ brain_games/scripts/main.py
@@ -1,6 +1,8 @@
 def main():
+    
     print(" Hello from brain-games!  ")
 
 
 if __name__ == "__main__":
+
     main()
