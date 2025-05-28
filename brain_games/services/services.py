99ec120d9bb138a01447648e0dcc47696b1c6795
vlrkors <vlrkors@gmail.com>
2025-05-28 22:39:00 +0300

    fix bug

--- brain_games/services/services.py
+++ brain_games/services/services.py
@@ -1,7 +1,4 @@
-from from secrets import SystemRandom
-
-def get_random_number():
-    return SystemRandom().randint(1, 100) import SystemRandom
+from secrets import SystemRandom
 
 def get_random_number():
     return SystemRandom().randint(1, 100)
