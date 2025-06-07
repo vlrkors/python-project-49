c6d7271ba99c505f8f2b13b28656481f741c3380
vlrkors <vlrkors@gmail.com>
2025-06-07 11:27:14 +0300

    feat: fix bugs

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,13 +1,13 @@
-from games.engine import run_game
-from games.gcd import get_question_and_answer
+from brain_games.games.engine import run_game
+from brain_games.games.gcd import generate_question_and_answer
 
 
-def start_game():
+def main():
     """
     Запускаем игру "Наибольший общий делитель (НОД)".
     """
     print("Find the greatest common divisor of given numbers.")
-    run_game(get_question_and_answer)
+    run_game(generate_question_and_answer)
 
 if __name__ == "__main__":
-    start_game()
+    main()
