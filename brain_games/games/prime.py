d27750bdab5d9162be79b37ae64d56b3ea9464a8
vlrkors <vlrkors@gmail.com>
2025-06-16 18:34:41 +0300

    feat: correct code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,5 +1,4 @@
 import random
-from brain_games.games.engine import is_prime
 
 
 def generate_question_and_answer():
@@ -8,3 +7,13 @@ def generate_question_and_answer():
     question = str(number)
     correct_answer = "yes" if is_prime(number) else "no"
     return question, correct_answer
+
+
+def is_prime(number):
+    """Проверяет, является ли число простым."""
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
