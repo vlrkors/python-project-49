a1eb02b83f151f3627c62f8999c1d07b6e45c4d0
vlrkors <vlrkors@gmail.com>
2025-06-16 18:06:29 +0300

    add prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,22 +1,11 @@
 import random
 from brain_games.games.engine import run_game
-from brain_games.services.services import is_prime
+from brain_games.games.prime import generate_question_and_answer
 
-
-def generate_question_and_answer():
-    """Генерирует вопрос и правильный ответ."""
-    number = random.randint(1, 100)  # Генерируем случайное число от 1 до 100
-    question = str(number)
-    correct_answer = "yes" if is_prime(number) else "no"
-    return question, correct_answer
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = input("May I have your name? ")
-    print(f"Hello, {name}!")
-    print("Answer 'yes' if given number is prime. Otherwise answer 'no'.")
-    
     run_game(generate_question_and_answer)
+
 
 if __name__ == "__main__":
     main()
