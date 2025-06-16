61ffac690a7e6a0125edf7adc5692e6cd5eef564
vlrkors <vlrkors@gmail.com>
2025-06-16 18:42:45 +0300

    final.

--- brain_games/services/services.py
+++ brain_games/services/services.py
@@ -4,13 +4,3 @@ from secrets import SystemRandom
 def get_random_number():
     
     return SystemRandom().randint(1, 100)
-
-
-def is_prime(number):
-    """Проверяет, является ли число простым."""
-    if number <= 1:
-        return False
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
