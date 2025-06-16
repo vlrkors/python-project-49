02bd020189ef9a0ed347f41148e55de613436a70
vlrkors <vlrkors@gmail.com>
2025-06-16 18:59:55 +0300

    final ruff linter

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,7 +1,8 @@
-from brain_games.games.engine import run_game, generate_progression
+from brain_games.games.engine import generate_progression, run_game
 
 
 def main():
+
     # Используем функцию generate_progression для генерации вопросов и ответов
     name = run_game(generate_progression)
     if name:
