61540d791033dc977d0fff0b3730949290b1c837
vlrkors <vlrkors@gmail.com>
2025-06-17 13:53:53 +0300

    correction code

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -4,9 +4,7 @@ from brain_games.games.engine import generate_progression, run_game
 def main():
 
     # Используем функцию generate_progression для генерации вопросов и ответов
-    name = run_game(generate_progression)
-    if name:
-        print(f"Congratulations, {name}!")
+    run_game(generate_progression)
 
 
 if __name__ == "__main__":
