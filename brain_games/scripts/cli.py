eb1cd5af383400aef2a5ece0f9e425bdf9c63ff6
vlrkors <vlrkors@gmail.com>
2025-06-17 16:12:45 +0300

    feaf: fix new string Welcome to the Brain Games!

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -3,6 +3,6 @@ import prompt
 
 def welcome_user():
     print("brain-games")
-    print("Welcome to brain-games!")
+    print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
