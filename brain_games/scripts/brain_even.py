210385c652254f666f361aa448479207326698dc
vlrkors <vlrkors@gmail.com>
2025-06-17 16:56:52 +0300

    fix bug

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,6 +5,7 @@ from ..games.services import get_random_number
 
 def main(): 
     # Вывод приветствия и запрос имени пользователя
+    print("brain-even\n")
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
